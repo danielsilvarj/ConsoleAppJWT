6486d3ac8cfcffdd360cd54d74c2a707ed01fe88
danielsilvarj <daniel.cg.rio@gmail.com>
2020-03-28 13:38:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppJWT/Program.cs
@@ -0,0 +1,36 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppJWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+            #region Create otken
+            var payload = new Dictionary<string, object>
+            {
+                { "claim1", 0 },
+                { "claim2", "claim2-value" }
+            };
+            
+            var token = new JwtBuilder()
+              .WithAlgorithm(new HMACSHA256Algorithm())
+              .WithSecret(secret)
+              .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+              .AddClaim("claim2", "claim2-value")
+              .Build();
+
+
+            Console.WriteLine(token);
+            Console.ReadKey();
+            #endregion
+            Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
